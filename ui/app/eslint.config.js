d4e680e85633b116844f614c2c673b2ec3184ca2
James Chapman <james.chapman@necsu.onmicrosoft.com>
2026-01-21 16:08:39 +0000

    stricter eslint

--- ui/app/eslint.config.js
+++ ui/app/eslint.config.js
@@ -1,24 +1,61 @@
-import eslintConfigPrettier from "eslint-config-prettier";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
 
 export default [
+  { ignores: ['dist'] },
+  js.configs.recommended,
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
+      parser: tsparser,
+      globals: globals.browser,
     },
     plugins: {
-      "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': tseslint,
     },
-    rules: {},
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-undef': 'off', // TypeScript handles this
+    },
   },
-  eslintConfigPrettier,
+  {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: globals.browser,
+    },
+    settings: {
+      react: { version: '18.3' },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    files: ['**/*.test.{ts,tsx}', '**/test-utils/**'],
+    languageOptions: {
+      globals: globals.jest,
+    },
+  },
+  prettier,
 ];
